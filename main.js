c92a895dc698eae60c97215383d9a4391b3453d8
davidsekielyk <david.sekielyk@gmail.com>
2019-04-09 00:25:56 -0300

    event-handling

--- main.js
+++ main.js
@@ -8,12 +8,23 @@ var app = new Vue({
         variants:[
             {
                 variantId:      2234,
-                variantColor:   "green"
+                variantColor:   "green",
+                variantImage:   "img/vmSocks-green-onWhite.jpg"
             },
             {
                 variantId:      2235,
-                variantColor:   "blue"
+                variantColor:   "blue",
+                variantImage:   "img/vmSocks-blue-onWhite.jpg"
             }
-        ]
+        ],
+        cart:       0
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        }
     }
 })
