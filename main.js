05b81e3c798fb96ef2656123bf5df893777c5799
davidsekielyk <david.sekielyk@gmail.com>
2019-04-09 00:05:28 -0300

    conditional rendering

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el:"#app",
     data:{
         product:    "Socks",
-        image:      "img/vmSocks-green-onWhite.jpg"
+        image:      "img/vmSocks-green-onWhite.jpg",
+        inventori:  11
     }
 })
