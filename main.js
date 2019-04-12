5d28d6f63dc97bbd9e79498c70432047105cd4e3
davidsekielyk <david.sekielyk@gmail.com>
2019-04-11 23:01:01 -0300

    computed properties

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 var app = new Vue({
     el:"#app",
     data:{
-        product:    "Socks",
-        image:      "img/vmSocks-green-onWhite.jpg",
-        inStock:    true,
-        details:    ["80% algodon","20% poliester","Marca Vue"],
+        product:        "Socks",
+        brand:          "Vue Mastery",
+        selectedVariant: 0,
+        details:        ["80% algodon","20% poliester","Marca Vue"],
         variants:[
             {
-                variantId:      2234,
-                variantColor:   "green",
-                variantImage:   "img/vmSocks-green-onWhite.jpg"
+                variantId:          2234,
+                variantColor:       "green",
+                variantImage:       "img/vmSocks-green-onWhite.jpg",
+                variantQuantity:    2
             },
             {
-                variantId:      2235,
-                variantColor:   "blue",
-                variantImage:   "img/vmSocks-blue-onWhite.jpg"
+                variantId:          2235,
+                variantColor:       "blue",
+                variantImage:       "img/vmSocks-blue-onWhite.jpg",
+                variantQuantity:    0
             }
         ],
         cart:       0
@@ -23,8 +25,19 @@ var app = new Vue({
         addToCart(){
             this.cart += 1
         },
-        updateImage(variantImage){
-            this.image = variantImage
+        updateImage(index){
+            this.selectedVariant = index
+        }
+    },
+    computed:{
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 })
