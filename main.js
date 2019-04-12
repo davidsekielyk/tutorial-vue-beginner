6480bfac9ef722a1d608ff18aa53caa7c23311c5
davidsekielyk <david.sekielyk@gmail.com>
2019-04-11 23:29:20 -0300

    components

--- main.js
+++ main.js
@@ -1,25 +1,69 @@
-var app = new Vue({
-    el:"#app",
-    data:{
-        product:        "Socks",
-        brand:          "Vue Mastery",
-        selectedVariant: 0,
-        details:        ["80% algodon","20% poliester","Marca Vue"],
-        variants:[
-            {
-                variantId:          2234,
-                variantColor:       "green",
-                variantImage:       "img/vmSocks-green-onWhite.jpg",
-                variantQuantity:    2
-            },
-            {
-                variantId:          2235,
-                variantColor:       "blue",
-                variantImage:       "img/vmSocks-blue-onWhite.jpg",
-                variantQuantity:    0
-            }
-        ],
-        cart:       0
+Vue.component("product",{
+    props:{
+        premium:{
+            type:Boolean,
+            required:true
+        }
+    },
+    template:`
+        <div class="product">
+            <div class="product-image">
+                <img :src="image" />
+            </div>
+            <div class="product-info">
+
+                <h1>{{title}}</h1>
+                
+                <p>Shipping {{ shipping }}</p>
+
+                <p v-if="inStock">inStock</p>
+                <p v-else>Out of Stock</p>
+                
+                <ul>
+                    <li v-for="detail in details">{{detail}}</li>
+                </ul>
+                
+                <div class="color-box"
+                    v-for="(variant, index) in variants" 
+                    :key="variant.variantId"
+                    :style="{ backgroundColor: variant.variantColor }"
+                    @mouseover="updateImage(index)">
+                </div>
+
+                <div class="cart">
+                    <p>Cart{{cart}}</p>
+                </div>
+                <!-- // usar v-on es lo mismo que @ -->
+                <button v-on:click="addToCart"
+                        :disabled="!inStock"
+                        :class="{ disabledButton: !inStock }">
+                    Add to cart
+                </button>
+            </div>
+        </div>
+    `,
+    data(){
+        return {
+            product:        "Socks",
+            brand:          "Vue Mastery",
+            selectedVariant: 0,
+            details:        ["80% algodon","20% poliester","Marca Vue"],
+            variants:[
+                {
+                    variantId:          2234,
+                    variantColor:       "green",
+                    variantImage:       "img/vmSocks-green-onWhite.jpg",
+                    variantQuantity:    2
+                },
+                {
+                    variantId:          2235,
+                    variantColor:       "blue",
+                    variantImage:       "img/vmSocks-blue-onWhite.jpg",
+                    variantQuantity:    0
+                }
+            ],
+            cart:       0
+        }
     },
     methods:{
         addToCart(){
@@ -38,6 +82,21 @@ var app = new Vue({
         },
         inStock(){
             return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping(){
+            if(this.premium){
+                return "Free"
+            }
+            else{
+                return 2.99
+            }
         }
+    }
+})
+
+var app = new Vue({
+    el:"#app",
+    data:{
+        premium:true
     }
 })
